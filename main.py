16a23405d08ba13fba391e3ac1f8e5f40b109ea5
K23HP <99390996+K23HP@users.noreply.github.com>
2023-06-24 01:15:39 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from my_math.algebra import Algebra
 
 algebra = Algebra(a=2, b=7, c=-15, d=13)
 print(f"x-value in first-degree algebra equation: {algebra.first_degree_x()}")
-print(f"x-value in second-degree algebra equation: {algebra.quadratic_x()}")
+print(f"(x1, x2) in quadratic equation: {algebra.quadratic_x()}")
